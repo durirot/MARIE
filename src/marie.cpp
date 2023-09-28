6cc40102a4d782d5093d672d7f37a12e1731370e
ryan <ryan@debian.wowzers.abc>
2023-09-28 12:51:23 -0400

    added in file reading

--- src/marie.cpp
+++ src/marie.cpp
@@ -2,6 +2,7 @@
 
 #include <fmt/core.h>
 #include <tuple>
+#include <cstdio>
 
 enum struct Instruction {
 	Load,
@@ -13,8 +14,11 @@ enum struct Instruction {
 struct Marie {
 	Marie(byte* image);
 
+	word run();
 	std::tuple<Instruction, word> decode();
 
+
+private:
 	byte memory[4096];
 	
 	word MAR;
@@ -27,6 +31,11 @@ Marie::Marie(byte* image)
 	
 }
 
+word Marie::run()
+{
+	return 0;
+}
+
 std::tuple<Instruction, word> Marie::decode()
 {
 
@@ -34,5 +43,28 @@ std::tuple<Instruction, word> Marie::decode()
 
 word marieLoad(const char* file)
 {
-	return 0;
+	FILE* handle = fopen(file, "rb");
+	if (handle == nullptr) {
+		fmt::print("Could not open file {}\n", file);
+		return -1;
+	}
+	fseek(handle, 0, SEEK_END);
+	size_t size = ftell(handle);
+	fseek(handle, 0, SEEK_SET);
+
+	byte* data = (byte*)malloc(size*sizeof(byte));
+	if (data == nullptr) {
+		fmt::print("Failed to allocate memory for file {}\n", file);
+		return -2;
+	}
+
+	(void)fread(data, size, 1, handle);
+	fclose(handle);
+
+	Marie vm(data);
+
+	word result = vm.run();
+
+	free(data);
+	return result;
 }
