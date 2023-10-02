32996ce260d1fdfb2f74f395e3381a4fccb2bf0a
Ryan <rmw071102@gmail.com>
2023-10-01 22:09:43 -0400

    Added in all marie registers

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@ int main(int argc, char** argv)
 {
 	if (argc < 2) {
 		fmt::print("Expected at least two arguments:\nUsage marievm <file>\n");
+		return -1;
 	}
 
 	return marieLoad(argv[2]);
--- src/marie.cpp
+++ src/marie.cpp
@@ -1,70 +1,73 @@
 #include "marie.h"
 
+#include <cstdio>
 #include <fmt/core.h>
 #include <tuple>
-#include <cstdio>
 
 enum struct Instruction {
-	Load,
-	Store,
-	Add,
-	Sub,
+    Load,
+    Store,
+    Add,
+    Sub,
 };
 
 struct Marie {
-	Marie(byte* image);
+    Marie(byte* image);
 
-	word run();
-	std::tuple<Instruction, word> decode();
-
+    word run();
+    std::tuple<Instruction, word> decode();
 
 private:
-	byte memory[4096];
-	
-	word MAR;
-	word MBR;
-	word AC;
+    byte memory[4096];
+
+    byte* image;
+
+    word AC; // Accumulator
+    word MAR; // Memory Address Register
+    word MBR; // Memory Buffer Register
+    word PC; // Program Counter
+    word IR; // Instruction Register (holds the next expression to be executed)
+    word InREG; //  Input Register (holds data from the input device)
 };
 
 Marie::Marie(byte* image)
+    : image(image)
 {
-	
 }
 
 word Marie::run()
 {
-	return 0;
+    return 0;
 }
 
 std::tuple<Instruction, word> Marie::decode()
 {
-
 }
 
 word marieLoad(const char* file)
 {
-	FILE* handle = fopen(file, "rb");
-	if (handle == nullptr) {
-		fmt::print("Could not open file {}\n", file);
-		return -1;
-	}
-	fseek(handle, 0, SEEK_END);
-	size_t size = ftell(handle);
-	fseek(handle, 0, SEEK_SET);
+    FILE* handle = fopen(file, "rb");
+    if (handle == nullptr) {
+        fmt::print("Could not open file {}\n", file);
+        return -1;
+    }
+    fseek(handle, 0, SEEK_END);
+    size_t size = ftell(handle);
+    fseek(handle, 0, SEEK_SET);
 
-	byte* data = (byte*)malloc(size*sizeof(byte));
-	if (data == nullptr) {
-		fmt::print("Failed to allocate memory for file {}\n", file);
-		return -2;
-	}
+    byte* data = (byte*)malloc(size * sizeof(byte));
+    if (data == nullptr) {
+        fmt::print("Failed to allocate memory for file {}\n", file);
+        return -2;
+    }
 
-	(void)fread(data, size, 1, handle);
-	fclose(handle);
+    (void)fread(data, size, 1, handle);
+    fclose(handle);
 
-	Marie vm(data);
+    Marie vm(data);
 
-	word result = vm.run();
+    word result = vm.run();
 
-	free(data);
-	return result;
+    free(data);
+    return result;
 }
